8916bdf816c516eba78b77eb302cdf9b4682f8a7
ap2343336-tech <ap2343336@gmail.com>
2026-09-18 13:06:45 -0500

    Create vector.cs

---
+++ vector.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vector
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] numeros = new int[5]
+            { 15, 42, 8, -4, 99 };
+            Console.WriteLine($"elemento en [0]: {numeros[0]}");
+
+        }
+    }
+}
